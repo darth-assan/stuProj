4074bbffba044cc547a3baaac4b6612ad4961f3e
Alvis Yim <alvisyim@gmail.com>
2024-12-06 03:46:59 +0100

    added a few print

--- src/sheet_03/task_03_local.py
+++ src/sheet_03/task_03_local.py
@@ -1,4 +1,4 @@
-debugmode = False
+debugmode = True
 import pandas as pd
 #import matplotlib.pyplot as plt
 import numpy as np
@@ -32,6 +32,8 @@ def select_k_for_beta(eigenvalues, beta):
 def project_data(data, eigenvectors, k):
     mean_centered_data = data - np.mean(data, axis=0) # centered the data to see corelation visually, but can see point of relevance
     top_eigenvectors = eigenvectors[:, :k]
+    if debugmode: print("\nprojection computation data shape:\n",data.shape)
+    if debugmode: print("\nprojection computation top_eigenv shape:\n",top_eigenvectors.shape)
     projected_data = np.dot(mean_centered_data, top_eigenvectors)  # the data is wrapped w.r.t. eigenvectors (selectively), new space where data is more deviated
     return projected_data
 
@@ -45,11 +47,10 @@ def pca_with_single_beta(data, beta):
     if debugmode: print("\ncov_matrix shape:\n",cov_matrix.shape)  # this matrix is symmetrical
     eigenvalues, eigenvectors = eigen_decomposition(cov_matrix)
     if debugmode: print("\neigenvectors and eigenvalues shapes:\n",eigenvectors.shape, eigenvalues.shape)
-    if debugmode: print("\neach eigenvectors and eigenvalues shapes:\n",eigenvectors.shape, eigenvalues.shape)
     k = select_k_for_beta(eigenvalues, beta)  # The number of components selected. = (len(output[0]))
     projected_data = project_data(data, eigenvectors, k)  # The data projected onto the top principal components.
     explained_variance_ratio = np.sum(eigenvalues[:k]) / np.sum(eigenvalues)  # explained_variance_ratio: The ratio of variance explained by the selected components.
-    return projected_data, explained_variance_ratio
+    return projected_data, explained_variance_ratio  # higher beta -> higher the ratio -> more data dimensions there will be
     
 
 def prepare_dataset(dir):
@@ -63,7 +64,7 @@ def prepare_dataset(dir):
             elif "train" in file_name.lower():  # Check if the file is a "train" file
                 dftrain_2103 = pd.concat([dftrain_2103, pd.read_csv(file_path)], ignore_index=True)
 
-    dftest_2103['time'] = range(0, len(dftest_2103) + 0)
+    dftest_2103['time'] = range(0, len(dftest_2103) + 0)  # will cause eigendecomposition on symbols
     dftrain_2103['time'] = range(0, len(dftrain_2103) + 0)
     return dftest_2103, dftrain_2103
 
@@ -74,7 +75,7 @@ dir = "..\\..\\data\\original\\hai-21.03"
 dftest_2103, dftrain_2103 = prepare_dataset(dir)
 betas = [0.998, 0.895, 0.879]
 for beta in betas:
-    print(f"\n---------- Beta = {beta} ----------")
+    if debugmode: print(f"\n---------- Beta = {beta} ----------")
     temp_pca, temp_evr = pca_with_single_beta(dftest_2103, beta)
     np.savetxt(f'hai_21.03_test_{beta}.csv', temp_pca, delimiter=",")
     temp_pca, temp_evr = pca_with_single_beta(dftrain_2103, beta)
